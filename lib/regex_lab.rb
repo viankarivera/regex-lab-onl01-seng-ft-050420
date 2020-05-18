9c03db826ef4cb7b825470b011fdc657dd425d74
Vianka Rivera <viankalrivera@gmail.com>
2020-05-18 13:34:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,5 @@
 def starts_with_a_vowel?(word)
-  starts_with_a_vowel?.each do |word|
-    word.match(/^[aeiou]/)
-  end
-
+word.match(/\A[aeiouAEIOU]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
