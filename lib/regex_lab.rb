cdb07f8ee92db798832453c571e753bf3c4a550b
Vianka Rivera <viankalrivera@gmail.com>
2020-05-18 13:42:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\A[A-Z]\w*\W\b/) != nil
 end
 
 def valid_phone_number?(phone)
